5e3111f20a878c5de8be54f6690fbb31138c1d01
wilcer1 <wille.cervin@live.se>
2022-08-21 14:58:24 +0200

    gitignore .env, added dependencies

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const dotenv = require('dotenv');
 
 app.get("/api", (req, res) => {
     res.json( {"hello": ["hi1", "hi2"]} );
