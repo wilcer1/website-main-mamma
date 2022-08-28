ac123e50ca72b329107e3225a3112ac2b476c761
wilcer1 <wille.cervin@live.se>
2022-08-28 16:37:59 +0200

    format function for api

--- client/src/components/Calendar.js
+++ client/src/components/Calendar.js
@@ -7,20 +7,13 @@ import "../style/Calendar.css"
 const ReactCalendar = () => {
   const [date, setDate] = useState(new Date());
   const [times, setTimes] = useState([]);
+
   useEffect(() => {
-    const timesformatted = []
+   
     fetch("/api/getAvailable")
       .then(res => res.json())
       .then(response => {
-        response.map((element) => {
-          var available = element.datetime.split("_");
-          timesformatted.push({
-            date: available[0],
-            time: available[1]
-          });
-
-        });
-        setTimes(timesformatted);
+        setTimes(response);
 
       });
 
@@ -42,11 +35,12 @@ const ReactCalendar = () => {
         <tbody>
           <tr>
             <td>
-              <Calendar onChange={onChange} value={date} minDate={new Date()} />
+              <Calendar onChange={onChange} value={date} minDate={new Date()} locale="sv" />
             </td></tr>
             <tr className='timestr'>
             
-         {times.map( time =>
+         {
+         times.map( time =>
          <td>
             <div className='times'>
             <button id={time.date} >{time.time}</button>
