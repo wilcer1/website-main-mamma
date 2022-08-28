9828e4595a5ceb4898be15278589edc09b0a8e26
wilcer1 <wille.cervin@live.se>
2022-08-28 13:42:14 +0200

    working on css

--- client/src/components/Calendar.js
+++ client/src/components/Calendar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Calendar from "react-calendar"
 import "../style/Calendar.css"
 
@@ -6,19 +6,46 @@ import "../style/Calendar.css"
 
 const ReactCalendar = () => {
   const [date, setDate] = useState(new Date());
-  const [times, setTimes] = useState([]);
+  const [times, setTimes] = useState(null);
+  useEffect(()=> {
+    const timesformatted = []
+    fetch("/api/getAvailable")
+    .then(res => res.json())
+    .then(response => {
+      response.map((element) => {
+        var available = element.datetime.split("_");
+        timesformatted.push({
+          date: available[0],
+          time: available[1]
+        });
+  
+      });
+      setTimes(timesformatted);
+      
+    });
+
+      
+  }, [])
+
   
 
   const onChange = date => {
-    console.log("hiyaaaa");
+    
     setDate(date)
   }
    
     return (
       <div className='container'>
+        <div>
         <Calendar onChange={onChange} value={date} minDate={new Date()}/>
-        {console.log(date)}
+        {console.log(times)}
+        </div>
+        <div className='times'>
+          <h1>HEY</h1>
+        </div>
       </div>
+      
+      
     );
 
 }
