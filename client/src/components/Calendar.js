b113496b855608077e9b9209c2ac78e33c4af453
wilcer1 <wille.cervin@live.se>
2022-08-28 14:14:29 +0200

    time css

--- client/src/components/Calendar.js
+++ client/src/components/Calendar.js
@@ -6,47 +6,61 @@ import "../style/Calendar.css"
 
 const ReactCalendar = () => {
   const [date, setDate] = useState(new Date());
-  const [times, setTimes] = useState(null);
-  useEffect(()=> {
+  const [times, setTimes] = useState([]);
+  useEffect(() => {
     const timesformatted = []
     fetch("/api/getAvailable")
-    .then(res => res.json())
-    .then(response => {
-      response.map((element) => {
-        var available = element.datetime.split("_");
-        timesformatted.push({
-          date: available[0],
-          time: available[1]
-        });
-  
-      });
-      setTimes(timesformatted);
-      
-    });
+      .then(res => res.json())
+      .then(response => {
+        response.map((element) => {
+          var available = element.datetime.split("_");
+          timesformatted.push({
+            date: available[0],
+            time: available[1]
+          });
 
-      
+        });
+        setTimes(timesformatted);
+
+      });
+
+
   }, [])
 
-  
+
 
   const onChange = date => {
-    
+
     setDate(date)
   }
-   
-    return (
-      <div className='container'>
-        <div>
-        <Calendar onChange={onChange} value={date} minDate={new Date()}/>
-        {console.log(times)}
-        </div>
-        <div className='times'>
-          <h1>HEY</h1>
-        </div>
-      </div>
-      
-      
-    );
+
+  return (
+    <div className='container'>
+      <table id='calendarandtime'>
+        <thead>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <Calendar onChange={onChange} value={date} minDate={new Date()} />
+            </td></tr>
+            <tr className='timestr'>
+            
+         {times.map( time =>
+         <td>
+            <div className='times'>
+            <button id={time.date} >{time.time}</button>
+          </div></td>)}
+          
+          </tr>
+          {console.log(times)}
+        </tbody>
+      </table>
+
+    </div>
+
+
+  );
 
 }
 
