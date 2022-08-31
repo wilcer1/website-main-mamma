2e2d524bb3676455a75ae7a9ca7662361fd05a25
Lucas Carlsson <lucas.carlsson@outlook.com>
2022-08-31 19:41:45 +0200

    fixed css animation for homepage
    Placeholder image and text

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,18 +1,86 @@
-import React from "react"
+import React, { useRef } from "react"
+import gsap from "gsap"
+import { ScrollTrigger } from "gsap/ScrollTrigger"
 import { useState } from "react"
 import { useEffect } from "react"
 import beach from "../assets/beach.png"
 import "../style/Home.css"
 
-function Home(){   
+function Home(){
+  const boxRef = useRef()
 
+  useEffect(() => {
+    // gsap.fromTo(
+    //   boxRef.current,
+    //   { y: 100, autoAlpha: 0 },
+    //   {
+    //     duration: 2.5,
+    //     y: 0,
+    //     autoAlpha: 1,
+    //     ease: "back",
+    //     overwrite: "auto"
+    //   }
+    // );
 
+    gsap.registerPlugin(ScrollTrigger);
+    gsap.utils.toArray(".revealUp").forEach(function (elem) {
+      ScrollTrigger.create({
+        trigger: elem,
+        start: "top 70%",
+        end: "bottom 20%",
+        onEnter: function () {
+          gsap.fromTo(
+            elem,
+            { y: 100, autoAlpha: 0 },
+            {
+              duration: 1.25,
+              y: 0,
+              autoAlpha: 1,
+              ease: "back",
+              overwrite: "auto"
+            }
+          );
+        },
+        onLeave: function () {
+          gsap.fromTo(elem, { autoAlpha: 1 }, { autoAlpha: 0, overwrite: "auto" });
+        },
+        onEnterBack: function () {
+          gsap.fromTo(
+            elem,
+            { y: -100, autoAlpha: 0 },
+            {
+              duration: 1.25,
+              y: 0,
+              autoAlpha: 1,
+              ease: "back",
+              overwrite: "auto"
+            }
+          );
+        },
+        onLeaveBack: function () {
+          gsap.fromTo(elem, { autoAlpha: 1 }, { autoAlpha: 0, overwrite: "auto" });
+        }
+      });
+    });
+  });
+  
   return (
-    
+    <>
     <div className="main-container">
-      
       {/* <img src={beach} className="hej" ></img> */}
-  </div>
+    </div>
+    <div className="box" ref={boxRef}>
+      <h1 className="home">
+        <span>There are no limits to what you can accomplish, except the limits you place on your own thinking.</span>
+        </h1></div>
+    <div className="placeholder">
+      <div className="revealUp">Testing</div>
+    </div>
+    <div className="placeholder2">
+      <div className="revealUp">Glazers Out</div>
+    </div>
+    
+    </>
 
   );
 };
