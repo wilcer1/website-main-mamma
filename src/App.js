f326bd6a8d13d316c091750155838fd01fbfddb2
wilcer1 <wille.cervin@live.se>
2022-09-04 16:53:59 +0200

    weqwqeeqw

--- src/App.js
+++ src/App.js
@@ -4,22 +4,19 @@ import Home from "./components/Home"
 import Navbar from "./components/Navbar"
 import Booking from "./components/Booking";
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter, Route } from "react-router-dom";
 import Admin from "./components/Admin";
 function App() {
   return (
     <>
     <Navbar/>
-    <Router>
-    
-      <Routes>
+    <HashRouter >
       
       <Route exact path='/' element={< Home />}/>
       <Route exact path='/boka' element={< Booking />}/>
       <Route exact path='/admin' element={< Admin />}/>
       
-      </Routes>
-    </Router>
+      </HashRouter>
     </>
   )
 }
