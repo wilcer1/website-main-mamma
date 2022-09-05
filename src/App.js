d08eb4819b01f4c1743b561d4660283d33e1ad0f
wilcer1 <wille.cervin@live.se>
2022-09-05 09:54:56 +0200

    ewq

--- src/App.js
+++ src/App.js
@@ -4,19 +4,19 @@ import Home from "./components/Home"
 import Navbar from "./components/Navbar"
 import Booking from "./components/Booking";
 
-import { Switch, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Admin from "./components/Admin";
 function App() {
   return (
     <>
     <Navbar/>
-    <Switch>
+    <Routes>
       
-      <Route exact path='/' element={Home}/>
-      <Route exact path='/boka' element={Booking}/>
-      <Route exact path='/admin' element={Admin}/>
+      <Route exact path='/' element={<Home/>}/>
+      <Route exact path='/boka' element={<Booking/>}/>
+      <Route exact path='/admin' element={<Admin/>}/>
       
-      </Switch>
+      </Routes>
     </>
   )
 }
